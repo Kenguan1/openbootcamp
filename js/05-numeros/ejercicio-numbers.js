34fe133aceb0a35229276949859b20cee61a2135
Kenguan1 <49210338+Kenguan1@users.noreply.github.com>
2022-12-27 15:30:25 -0500

    Create ejercicio-numbers.js

---
+++ js/05-numeros/ejercicio-numbers.js
@@ -0,0 +1,12 @@
+let altura=170
+let alturaM=1.70
+let peso=60.5
+
+const altura_aprox= Math.ceil(alturaM) 
+console.log(altura_aprox)
+
+const peso_aprox= Math.floor (peso)
+console.log(peso_aprox)
+
+const iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(iguales)
