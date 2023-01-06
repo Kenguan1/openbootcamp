c8282a28e07cde6543f70a183af46dd842e632f1
Kenguan1 <49210338+Kenguan1@users.noreply.github.com>
2023-01-06 14:56:34 -0500

    Update indexejer.js

--- js/10-modulos-en-node/indexejer.js
+++ js/10-modulos-en-node/indexejer.js
@@ -5,3 +5,9 @@ const resultadoMul = multiplica(suma(1,2), suma(4,5))
 // console.log(resultadoMul)
 
 console.log(chalk.green(resultadoMul));
+
+
+//PARA ESTE EJERCICIO:
+// primero creamos la carpeta ejercicio, dentro de la carpeta hicimos el "npm init -y"
+// luego al package.json le agregamos el "type": "module",
+// luego cuando ya teniamos el import y export de las funciones listos corremos el codigo con "node nombrecodigo.js" en este caso indexejer.js
