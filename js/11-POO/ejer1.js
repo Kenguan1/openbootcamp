42f160e6141f89319ed9214d854ee1b007246183
Kenguan1 <49210338+Kenguan1@users.noreply.github.com>
2023-01-07 16:32:24 -0500

    Create ejer1.js

---
+++ js/11-POO/ejer1.js
@@ -0,0 +1,25 @@
+class Estudiante {
+   nombre = "Juanito"
+   asignaturas = ["Javascript", "HTML", "CSS"]
+    
+   obtenDatos(){
+        return {
+            nombreeeee: this.nombre, //siempre que queramos acceder a un atributo dentro de la clase tenemos que poner this.atributo
+            asignaturasssss: this.asignaturas
+        }
+    }
+}
+
+const estudiante1 = new Estudiante()
+console.log(estudiante1.obtenDatos())
+
+
+// const crearEstudiante = (nombre, asignaturas) =>{
+//     return {
+//         nombre,
+//         asignaturas,
+//     }
+// }
+
+// const nuevoEstu = crearEstudiante("emilio", ["aaa", "bbb", "ccc"])
+// console.log(nuevoEstu)
